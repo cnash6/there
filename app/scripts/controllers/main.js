216178fb901911b8cb9a7a4ffac981f6a3121204
Christian Nash <christiann@slalom.com>
2016-04-02 17:22:19 -0400

    initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name thereApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the thereApp
+ */
+angular.module('thereApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
