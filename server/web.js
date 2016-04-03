3536e7014e80f90133770567c1becc13c203aaf5
Christian Nash <christiann@slalom.com>
2016-04-02 20:35:45 -0400

    one more try on heroku

--- server/web.js
+++ server/web.js
@@ -3,11 +3,28 @@
 //   var app = express();
 //    app.use(gzippo.staticGzip("" + __dirname + "/dist"));
 //   app.listen(process.env.PORT || 5000);
-var gzippo = require('gzippo');
+
+
+
+// var gzippo = require('gzippo');
+// var express = require('express');
+// var morgan = require('morgan');
+// var app = express();
+
+// app.use(morgan('dev'));
+// app.use(gzippo.staticGzip("" + __dirname + "/dist"));
+// app.listen(process.env.PORT || 5000);
+
 var express = require('express');
-var morgan = require('morgan');
 var app = express();
 
-app.use(morgan('dev'));
-app.use(gzippo.staticGzip("" + __dirname + "/dist"));
-app.listen(process.env.PORT || 5000);
+// app.get('/', function (req, res) {
+//   res.send('Hello World!');
+// });
+
+app.listen(process.env.PORT || 5000, function () {
+	var port = process.env.PORT || 5000;
+	console.log("server started on port " + port);
+	app.use(express.static('dist'));
+});
+
