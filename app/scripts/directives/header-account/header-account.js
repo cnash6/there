6e4279c9af12575f66bcb0d642ab3d86588e0371
Will Riley <wanderingwill@gmail.com>
2016-04-03 12:42:33 -0400

    no message

--- app/scripts/directives/header-account/header-account.js
+++ app/scripts/directives/header-account/header-account.js
@@ -7,11 +7,13 @@ angular.module('thereApp')
     templateUrl: 'scripts/directives/header-account/header-account.html',
     controller: function($scope, api, moment, auth, $rootScope) {
 
+      //$scope.currentUser = auth.getCurrentUser();
+
       $rootScope.$on('changeCurrentUser', function() {
 
         console.log('changeCurrentUser');
         $scope.currentUser = auth.getCurrentUser();
-        
+
       });
 
       $scope.loggy = function() {
