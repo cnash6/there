784afb58f81aaf6e3e944ee29422e9fc496c95b7
Will Riley <wanderingwill@gmail.com>
2016-04-02 21:03:55 -0400

    no message

--- app/scripts/controllers/session.js
+++ app/scripts/controllers/session.js
@@ -10,21 +10,28 @@
 angular.module('thereApp')
   .controller('SessionCtrl', function () {
 
-
       var apiKey = '45548832';
-      var sessionId = '2_MX40NTU0ODgzMn5-MTQ1OTYwNDg0NDg0N35EU3U1cktYY2lhTEJHc3VPQTNXNFY4NGR-UH4'; 
+      var sessionId = '2_MX40NTU0ODgzMn5-MTQ1OTYwNDg0NDg0N35EU3U1cktYY2lhTEJHc3VPQTNXNFY4NGR-UH4';
       var token = 'T1==cGFydG5lcl9pZD00NTU0ODgzMiZzaWc9M2EzMzhhOWUxYWJjNDYzOTZhZmNkMTQ5YjY1N2U0MjQ4YjlkNzYyOTpyb2xlPXB1Ymxpc2hlciZzZXNzaW9uX2lkPTJfTVg0ME5UVTBPRGd6TW41LU1UUTFPVFl3TkRnME5EZzBOMzVFVTNVMWNrdFlZMmxoVEVKSGMzVlBRVE5YTkZZNE5HUi1VSDQmY3JlYXRlX3RpbWU9MTQ1OTYwNTUwNiZub25jZT0wLjI3NzgzNzkxODQ5MTY1ODImZXhwaXJlX3RpbWU9MTQ1OTY5MTkwNg==';
-      var session = OT.initSession(apiKey, sessionId); 
-      session.on({ 
-          streamCreated: function(event) { 
-            session.subscribe(event.stream, 'subscribersDiv', {insertMode: 'append'}); 
-          } 
-      }); 
+      var session = OT.initSession(apiKey, sessionId);
+
+      session.on({
+          streamCreated: function(event) {
+            session.subscribe(event.stream, 'myCamDiv', {
+              insertMode: 'append'
+            });
+          }
+      });
       session.connect(token, function(error) {
         if (error) {
           console.log(error.message);
         } else {
-          	session.publish('myPublisherDiv', {width: '100%', height: '600px'}); 
+          	session.publish('theirCamDiv', {
+              width: '100%',
+              height: '600px'
+            });
         }
       });
+
+
   });
