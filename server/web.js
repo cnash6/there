31c9a9a54b6ca2f0264b8a1795129c6adce67f5d
Christian Nash <christiann@slalom.com>
2016-04-02 20:55:15 -0400

    get sessions

--- server/web.js
+++ server/web.js
@@ -1,30 +1,32 @@
-// var gzippo = require('gzippo');
-//   var express = require('express');
-//   var app = express();
-//    app.use(gzippo.staticGzip("" + __dirname + "/dist"));
-//   app.listen(process.env.PORT || 5000);
-
-
-
-// var gzippo = require('gzippo');
-// var express = require('express');
-// var morgan = require('morgan');
-// var app = express();
-
-// app.use(morgan('dev'));
-// app.use(gzippo.staticGzip("" + __dirname + "/dist"));
-// app.listen(process.env.PORT || 5000);
-
 var express = require('express');
 var app = express();
 
-// app.get('/', function (req, res) {
-//   res.send('Hello World!');
-// });
+var API_KEY = "45548832";
+var SECRET = "dd48288c2fc7807cb08dcec77009c0fd45efe205";
+
+var OpenTok = require('opentok');
+var opentok = new OpenTok(API_KEY, SECRET);
 
 app.listen(process.env.PORT || 5000, function () {
 	var port = process.env.PORT || 5000;
 	console.log("server started on port " + port);
 	app.use(express.static('dist'));
 });
+
+app.get("/getsession", function(req, res) {
+	console.log(opentok);	
+	var sessionId;
+	opentok.createSession({mediaMode:"routed"}, function(error, session) {
+	  if (error) {
+	    console.log("Error creating session:", error)
+	  } else {
+	    sessionId = session.sessionId;
+	    var token = opentok.generateToken(sessionId);
+	    console.log("Session ID: " + sessionId);
+	    res.send("SessionId: " + sessionId + "\n\nToken: " + token);
+	  }
+	});
+})
+
+
 
