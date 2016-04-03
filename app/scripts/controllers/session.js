c7c8fad25335b7535a84d8851d64666ef3313dac
Christian Nash <christiann@slalom.com>
2016-04-03 03:09:40 -0400

    multi session

--- app/scripts/controllers/session.js
+++ app/scripts/controllers/session.js
@@ -18,10 +18,8 @@ angular.module('thereApp')
   		// }, function myError(response) {
   		//     console.log(response.statusText);
   		// });
-		var apiKey = '45548832';
+		var apikey = '45548832';
 		var sessionId = '2_MX40NTU0ODgzMn5-MTQ1OTYwNDg0NDg0N35EU3U1cktYY2lhTEJHc3VPQTNXNFY4NGR-UH4';
-		var token = 'T1==cGFydG5lcl9pZD00NTU0ODgzMiZzaWc9M2EzMzhhOWUxYWJjNDYzOTZhZmNkMTQ5YjY1N2U0MjQ4YjlkNzYyOTpyb2xlPXB1Ymxpc2hlciZzZXNzaW9uX2lkPTJfTVg0ME5UVTBPRGd6TW41LU1UUTFPVFl3TkRnME5EZzBOMzVFVTNVMWNrdFlZMmxoVEVKSGMzVlBRVE5YTkZZNE5HUi1VSDQmY3JlYXRlX3RpbWU9MTQ1OTYwNTUwNiZub25jZT0wLjI3NzgzNzkxODQ5MTY1ODImZXhwaXJlX3RpbWU9MTQ1OTY5MTkwNg==';
-
 
 		if($location.search().sessionid) {
 			sessionId = $location.search().sessionid;
@@ -36,35 +34,41 @@ angular.module('thereApp')
   		    	role: 'client'
   		    }
   		}).then(function mySucces(response) {
-  		    token = response.token;
+  		    var token = response.data.token;
+  		    initSession(apikey, sessionId, token)
+
   		}, function myError(response) {
   		    console.log(response.statusText);
+  		    var token = 'T1==cGFydG5lcl9pZD00NTU0ODgzMiZzaWc9M2EzMzhhOWUxYWJjNDYzOTZhZmNkMTQ5YjY1N2U0MjQ4YjlkNzYyOTpyb2xlPXB1Ymxpc2hlciZzZXNzaW9uX2lkPTJfTVg0ME5UVTBPRGd6TW41LU1UUTFPVFl3TkRnME5EZzBOMzVFVTNVMWNrdFlZMmxoVEVKSGMzVlBRVE5YTkZZNE5HUi1VSDQmY3JlYXRlX3RpbWU9MTQ1OTYwNTUwNiZub25jZT0wLjI3NzgzNzkxODQ5MTY1ODImZXhwaXJlX3RpbWU9MTQ1OTY5MTkwNg==';
+  		    initSession(apikey, sessionId)
+
   		});
 
 
 
 	  	//console.log($location.search());
 
-
-		var session = OT.initSession(apiKey, sessionId);
-		$rootScope.session = session;
-		session.on({
-		  streamCreated: function(event) {
-		    session.subscribe(event.stream, 'theirCamDiv', {
-		      insertMode: 'append'
-		    });
-		  }
-		});
-		session.connect(token, function(error) {
-			if (error) {
-			  console.log(error.message);
-			} else {
-			  	session.publish('myCamDiv', {
-			      width: '100%',
-			      height: '600px',
-			      name: 'client'
+		function initSession(apikey, sessionid, token) {
+			var session = OT.initSession(apikey, sessionId);
+			$rootScope.session = session;
+			session.on({
+			  streamCreated: function(event) {
+			    session.subscribe(event.stream, 'theirCamDiv', {
+			      insertMode: 'append'
 			    });
-			}
-		});
+			  }
+			});
+			session.connect(token, function(error) {
+				if (error) {
+				  console.log(error.message);
+				} else {
+				  	session.publish('myCamDiv', {
+				      width: '100%',
+				      height: '600px',
+				      name: 'client'
+				    });
+				}
+			});
+		}
 
   	});
